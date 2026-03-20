2d5d1d4b8d3c94890bef9d6e7b82688a97adb174
Breno <brematosmene@gmail.com>
2026-03-20 18:56:01 -0300

    adiciona testes para o struct Grid

--- src/core/grid.rs
+++ src/core/grid.rs
@@ -39,9 +39,146 @@ pub trait GridBlocos {
                     continue;
                 }
 
-                let (x, y) = pos_checada.into();
-                self.posicoes[y as usize][x as usize] = *bloco;
+                self.posicionar_bloco(*bloco, pos_checada);
             }
         }
+    }
+}
+
+const TAM_MAX_GRID: usize = 50;
+
+type Posicoes = [[Bloco; TAM_MAX_GRID]; TAM_MAX_GRID];
+const POSICOES_BASE: Posicoes = [[0; TAM_MAX_GRID]; TAM_MAX_GRID];
+
+pub struct Grid {
+    posicoes: Posicoes,
+    largura: usize,
+    altura: usize,
+}
+
+impl Grid {
+    fn new(altura: usize, largura: usize) -> Option<Self> {
+        if altura > TAM_MAX_GRID || largura > TAM_MAX_GRID {
+            None
+        } else {
+            Some(Grid {
+                largura,
+                altura,
+                posicoes: POSICOES_BASE,
+            })
+        }
+    }
+}
+
+impl GridBlocos for Grid {
+    fn dimensoes(&self) -> IVec2 {
+        IVec2 {
+            x: self.largura as i32,
+            y: self.altura as i32,
+        }
+    }
+    fn posicao_ocupada(&self, pos: IVec2) -> bool {
+        if self.fora_dos_limites(pos) {
+            return false;
+        }
+        self.posicoes[pos.y as usize][pos.x as usize] != 0
+    }
+
+    fn posicionar_bloco(&mut self, bloco: Bloco, pos: IVec2) {
+        if self.fora_dos_limites(pos) {
+            return;
+        }
+        self.posicoes[pos.y as usize][pos.x as usize] = bloco;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn posicionar_blocos_dentro_grid() {
+        let mut grid = Grid::new(3, 3).expect("grid deveria ter iniciado");
+        let mut esperado = POSICOES_BASE;
+        let mut bloco = BLOCOS_BASE;
+        let tam_bloco = 3;
+
+        for i in 0..tam_bloco {
+            // cria uma peça 000 111 000
+            bloco[1][i] = 1;
+            esperado[1][i] = 1;
+        }
+
+        grid.posicionar_blocos(&bloco, tam_bloco, IVec2::new(0, 0));
+
+        assert_eq!(grid.posicoes, esperado)
+    }
+
+    #[test]
+    fn nao_deixar_posicionar_fora_do_grid() {
+        let grid = Grid::new(3, 3).expect("grid deveria ter iniciado");
+        let mut bloco = BLOCOS_BASE;
+        let tam_bloco = 3;
+
+        (0..tam_bloco).for_each(|i| {
+            // cria uma peça 000 111 000
+            bloco[1][i] = 1;
+        });
+
+        let pode_posicionar = grid.pode_posicionar(&bloco, tam_bloco, IVec2::new(1, 1));
+
+        assert!(!pode_posicionar)
+    }
+
+    #[test]
+    fn nao_deixar_posicionar_encima_de_bloco() {
+        let mut grid = Grid::new(3, 3).expect("grid deveria ter iniciado");
+        let mut bloco = BLOCOS_BASE;
+        let tam_bloco = 3;
+
+        (0..tam_bloco).for_each(|i| {
+            // cria uma peça 000 111 000
+            bloco[1][i] = 1;
+        });
+
+        grid.posicoes[1][1] = 2;
+
+        let pode_posicionar = grid.pode_posicionar(&bloco, tam_bloco, IVec2::new(0, 0));
+
+        assert!(!pode_posicionar)
+    }
+
+    #[test]
+    fn deixar_posicionar() {
+        let grid = Grid::new(3, 3).expect("grid deveria ter iniciado");
+        let mut bloco = BLOCOS_BASE;
+        let tam_bloco = 3;
+
+        (0..tam_bloco).for_each(|i| {
+            // cria uma peça 000 111 000
+            bloco[1][i] = 1;
+        });
+
+        let pode_posicionar = grid.pode_posicionar(&bloco, tam_bloco, IVec2::new(0, 0));
+
+        assert!(pode_posicionar)
+    }
+
+    #[test]
+    fn deixar_posicionar_com_padding() {
+        let grid = Grid::new(3, 3).expect("grid deveria ter iniciado");
+        let mut bloco = BLOCOS_BASE;
+        let tam_bloco = 4;
+
+        (1..tam_bloco).for_each(|i| {
+            // cria uma peça 0000 0111 0000 0000
+            bloco[1][i] = 1;
+        });
+
+        // grid.posicoes[1][1] = 2;
+
+        let pode_posicionar = grid.pode_posicionar(&bloco, tam_bloco, IVec2::new(-1, 0));
+
+        assert!(pode_posicionar)
     }
 }
